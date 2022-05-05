ca9ff1de9db77d558107e7e31d560562af94ffea
Andrew Glancy <andrew.glancy@door3.com>
2022-05-05 16:02:27 -0400

    Finished Module 4

--- BugTrackerUI/Startup.cs
+++ BugTrackerUI/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -28,6 +28,7 @@ namespace BugTrackerUI
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddSingleton<IBugService, BugService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
